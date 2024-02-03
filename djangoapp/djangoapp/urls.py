adbe96f7401f19aa0df2081f2629e12053db0abe
Dorothy princy maria G <dorothyprincy2002@gmail.com>
2024-02-03 13:01:14 +0530

    second commit

--- djangoapp/djangoapp/urls.py
+++ djangoapp/djangoapp/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from loginsystem.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('loginsystem/', include('loginsystem.urls')),
-   
+    path('firstpage/',firstpage),
+    path('',firstpage)
 ]
